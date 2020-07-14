da29b73e14401401afa9023c63d1d9b7ba7c7ac9
Harryx2019 <1204277702@qq.com>
2020-07-14 09:29:10 +0800

    初步改进

--- main.cpp
+++ main.cpp
@@ -1,61 +1,6 @@
 #include <iostream>
-#include <cmath>
+#include "account.h"
 using namespace std;
-class SavingsAccount {   //储蓄账户类
-private:
-	int id;              //账号
-	double balance;      //余额
-	double rate;         //存款的年利率
-	int lastDate;        //上次变更余额的时期
-	double accumulation; //余额按日累加之和
-	//记录一笔账，date为日期，amount为金额，desc为说明
-	void record(int date, double amount);
-	//获得到指定日期为止的存款金额按日积累值
-	double accumulate(int date) const {
-		return accumulation + balance * (date - lastDate);
-	}
-public:
-	//构造函数
-	SavingsAccount(int date, int id, double rate);
-	int getId() { return id; }
-	double getBalance() { return balance; }
-	double getRate() { return rate; }
-	void deposit(int date, double amount);    //存入现金
-	void withdraw(int date, double amount);   //去取出现金
-	//结算利息，每年1月1日调用一次该函数
-	void settle(int date);
-	//显示账户信息
-	void show();
-};
-//SavingsAccount类相关成员函数的实现
-SavingsAccount::SavingsAccount(int date, int id, double rate) :id(id), balance(0), rate(rate), lastDate(date), accumulation(0) {
-	cout << date << "\t#" << id << " is created" << endl;
-}
-void SavingsAccount::record(int date, double amount) {
-	accumulation = accumulate(date);
-	lastDate = date;
-	amount = floor(amount * 100 + 0.5) / 100;
-	balance += amount;
-	cout << date << "\t#" << id << "\t" << amount << "\t" << balance << endl;
-}
-void SavingsAccount::deposit(int date, double amount) {
-	record(date, amount);
-}
-void SavingsAccount::withdraw(int date, double amount) {
-	if (amount > getBalance())
-		cout << "Error: not enough money" << endl;
-	else
-		record(date, -amount);
-}
-void SavingsAccount::settle(int date) {
-	double interest = accumulate(date) * rate / 365;//计算年息
-	if (interest != 0)
-		record(date, interest);
-	accumulation = 0;
-}
-void SavingsAccount::show() {
-	cout << "#" << id << "\tbalance:" << balance;
-}
 
 int main() {
 	//建立几个账户
@@ -74,5 +19,6 @@ int main() {
 	cout << endl;
 	sa1.show();
 	cout << endl;
+	cout << "Total: " << SavingsAccount::getTotal() << endl;
 	return 0;
 }
