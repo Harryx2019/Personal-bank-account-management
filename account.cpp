d154218a8a450ed123a9e77ad8dcef7443e51400
Harryx2019 <1204277702@qq.com>
2020-07-15 09:36:14 +0800

    全面完善

--- account.cpp
+++ account.cpp
@@ -1,36 +1,91 @@
 #include "account.h"
 #include <cmath>
 #include <iostream>
+#include <utility>
 using namespace std;
+using namespace std::rel_ops;
 
-double SavingsAccount::total = 0;
-//SavingsAccount类相关成员函数的实现
-SavingsAccount::SavingsAccount(int date, int id, double rate) :id(id), balance(0), rate(rate), lastDate(date), accumulation(0) {
-	cout << date << "\t#" << id << " is created" << endl;
+//AccountRecord类的实现
+AccountRecord::AccountRecord(const Date &date,const Account *account,double amount,double balance,const std::string &desc):date(date),account(account),amount(amount),balance(balance),desc(desc){ }
+void AccountRecord::show() const {
+	cout << date << "\t#" << account->getId() << "\t" << amount << "\t" << balance << "\t" << desc << endl;
 }
-void SavingsAccount::record(int date, double amount) {
-	accumulation = accumulate(date);
-	lastDate = date;
+
+double Account::total = 0;
+RecordMap Account::recordMap;
+
+//Account类的实现
+Account::Account(const Date& date, const string& id) :id(id), balance(0) {
+	cout << date << "\t#" << id << " created" << endl;
+}
+void Account::record(const Date& date, double amount, const string& desc) {
 	amount = floor(amount * 100 + 0.5) / 100;
 	balance += amount;
 	total += amount;
-	cout << date << "\t#" << id << "\t" << amount << "\t" << balance << endl;
+	date.show();
+	cout << "\t#" << id << "\t" << amount << "\t" << balance << "\t" << desc << endl;
 }
-void SavingsAccount::deposit(int date, double amount) {
-	record(date, amount);
+void Account::show(ostream &out) const {
+	out << id << "\tbalance:" << balance;
 }
-void SavingsAccount::withdraw(int date, double amount) {
+void Account::error(const string& msg) const {
+	throw AccountException(this, msg);
+}
+void Account::query(const Date& begin, const Date& end) {
+	if (begin <= end) {
+		RecordMap::iterator iter1 = recordMap.lower_bound(begin);
+		RecordMap::iterator iter2 = recordMap.upper_bound(end);
+		for (RecordMap::iterator iter = iter1; iter != iter2; ++iter)
+			iter->second.show();
+	}
+}
+
+//SavingsAccount类相关成员函数的实现
+SavingsAccount::SavingsAccount(const Date& date, const string& id, double rate) :Account(date,id),rate(rate),acc(date,0) { }
+void SavingsAccount::deposit(const Date &date, double amount, const string& desc) {
+	record(date, amount,desc);
+	acc.change(date, getBalance());
+}
+void SavingsAccount::withdraw(const Date& date, double amount, const string& desc) {
 	if (amount > getBalance())
-		cout << "Error: not enough money" << endl;
-	else
-		record(date, -amount);
+		error("not enough money");
+	else {
+		record(date, -amount,desc);
+		acc.change(date, getBalance());
+	}
 }
-void SavingsAccount::settle(int date) {
-	double interest = accumulate(date) * rate / 365;//计算年息
+void SavingsAccount::settle(const Date& date) {
+	if (date.getMonth() == 1) {
+		double interest = acc.getSum(date) * rate / (date - Date(date.getYear() - 1, 1, 1));//计算年息
+		if (interest != 0)
+			record(date, interest, "interest");
+		acc.reset(date, getBalance());
+	}
+}
+
+//CreditAccount类相关成员函数的实现
+CreditAccount::CreditAccount(const Date &date,const string &id,double credit,double rate,double fee):Account(date,id),credit(credit),rate(rate),fee(fee),acc(date,0){ }
+void CreditAccount::deposit(const Date& date, double amount, const string& desc) {
+	record(date, amount, desc);
+	acc.change(date, getDebt());
+}
+void CreditAccount::withdraw(const Date& date, double amount, const string& desc) {
+	if (amount-getBalance() > credit)
+		error("not enough credit");
+	else {
+		record(date, -amount, desc);
+		acc.change(date, getDebt());
+	}
+}
+void CreditAccount::settle(const Date& date) {
+	double interest = acc.getSum(date) * rate;
 	if (interest != 0)
-		record(date, interest);
-	accumulation = 0;
+		record(date, interest, "interest");
+	if (date.getMonth() == 1)
+		record(date, -fee, "annual fee");
+	acc.reset(date, getDebt());
 }
-void SavingsAccount::show() {
-	cout << "#" << id << "\tbalance:" << balance;
+void CreditAccount::show(ostream& out) const {
+	Account::show(out);
+	out << "\tAvailable credit:" << getAvailableCredit();
 }
--- account.h
+++ account.h
@@ -1,29 +1,106 @@
 #pragma once
-class SavingsAccount {   //储蓄账户类
+#include "date.h"
+#include "accumulator.h"
+#include <string>
+#include <map>
+#include <istream>
+#include <stdexcept>
+
+class Account; //前置声明
+class AccountRecord {  //账目记录
 private:
-	int id;              //账号
+	Date date;  //日期
+	const Account* account;  //账户
+	double amount;    //金额
+	double balance;   //余额
+	std::string desc; //描述
+public:
+	//构造函数
+	AccountRecord(const Date& date, const Account* account, double amount, double balance, const std::string& desc);
+	void show() const;  //输出当前记录
+};
+//定义用来存储账目记录的多重映射类型
+typedef std::multimap<Date, AccountRecord> RecordMap;
+class Account { //账户类
+private:
+	std::string id;              //账号
 	double balance;      //余额
-	double rate;         //存款的年利率
-	int lastDate;        //上次变更余额的时期
-	double accumulation; //余额按日累加之和
 	static double total; //所有账户的总金额
+	static RecordMap recordMap;  //账目记录
+protected:
+	//供派生类调用的构造函数，id为账号
+	Account(const Date& date, const std::string& id);
 	//记录一笔账，date为日期，amount为金额，desc为说明
-	void record(int date, double amount);
-	//获得到指定日期为止的存款金额按日积累值
-	double accumulate(int date) const {
-		return accumulation + balance * (date - lastDate);
+	void record(const Date& date, double amount, const std::string& desc);
+	//报告错误信息
+	void error(const std::string& msg) const;
+public:
+	const std::string& getId() const { return id; }
+	double getBalance() const { return balance; }
+	static double getTotal() { return total; }
+	//存入现金，date为日期，amount为金额，desc为款项说明
+	virtual void deposit(const Date& date, double amount, const std::string& desc) = 0;
+	//取出现金，date为日期，amount为金额，desc为款项说明
+	virtual void withdraw(const Date& date, double amount, const std::string& desc) = 0;
+	//结算（计算利息、年费等），每月计算一次，date为结算日期
+	virtual void settle(const Date& date) = 0;
+	//显示账户信息
+	virtual void show(std::ostream& out) const;
+	static void query(const Date& begin, const Date& end); //查询指定时间内的账目记录
+};
+inline std::ostream& operator<<(std::ostream& out, const Account& account) {
+	account.show(out);
+	return out;
+}
+
+class SavingsAccount :public Account {   //储蓄账户类
+private:
+	Accumulator acc;     //辅助计算利息的累加器
+	double rate;         //存款的年利率
+public:
+	//构造函数
+	SavingsAccount(const Date &date, const std::string &id, double rate);
+	double getRate() const { return rate; }
+	void deposit(const Date &date, double amount,const std::string &desc);    //存入现金
+	void withdraw(const Date& date, double amount, const std::string& desc);   //取出现金
+	//结算利息，每年1月1日调用一次该函数
+	void settle(const Date &date);
+};
+
+class CreditAccount : public Account {  //信用账户类
+private:
+	Accumulator acc;  //辅助计算利息的累加器
+	double credit;    //信用额度
+	double rate;      //欠款的日利率
+	double fee;       //信用卡年费
+	double getDebt() const {  //获得欠款额
+		double balance = getBalance();
+		return (balance < 0 ? balance : 0);
 	}
 public:
 	//构造函数
-	SavingsAccount(int date, int id, double rate);
-	int getId() const { return id; }
-	double getBalance() const { return balance; }
+	CreditAccount(const Date& date, const std::string& id, double credit, double rate, double fee);
+	double getCredit() const { return credit; }
 	double getRate() const { return rate; }
-	static double getTotal() { return total; }
-	void deposit(int date, double amount);    //存入现金
-	void withdraw(int date, double amount);   //去取出现金
-	//结算利息，每年1月1日调用一次该函数
-	void settle(int date);
-	//显示账户信息
-	void show();
+	double getFee() const { return fee; }
+	double getAvailableCredit() const {  //获得可用信用额度
+		if (getBalance() < 0)
+			return credit + getBalance();
+		else
+			return credit;
+	}
+	//存入现金
+	void deposit(const Date& date, double amount, const std::string& desc);
+	//取出现金
+	void withdraw(const Date& date, double amount, const std::string& desc);
+	void settle(const Date& date);  //结算利息和年费，每月1日调用一次该函数
+	void show(std::ostream& out) const;
+};
+
+class AccountException :public std::runtime_error {
+private:
+	const Account* account;
+public:
+	AccountException(const Account *account,const std::string msg):runtime_error(msg),account(account){ }
+	const Account* getAccount() const { return account; }
 };
